5deab1f78be0f20da16fbfe35e7d47ffa55f89c7
sasaki117 <r-sasaki@fores.jp>
2024-11-28 10:36:55 +0900

    Lesson05

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,37 @@
+package baseball;
+
+public class BaseBallTeam {
+    public String name;
+    public int win;
+    public int lose;
+    public int draw;
+
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+    }
+
+    public double getRate() {
+        return (double) win / (win + lose);
+    }
+
+    public void report() {
+        System.out.println(name + " の2022年の成績は " + win + "勝 " + lose + "敗 " + draw + "分、勝率は " + getRate() + "です。");
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getWin() {
+        return win;
+    }
+    public int getLose() {
+        return lose;
+    }
+    public int getDraw() {
+        return draw;
+    }
+
+}
